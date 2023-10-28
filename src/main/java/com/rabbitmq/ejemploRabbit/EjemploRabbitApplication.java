b3b79093abeb1690b60bed5dd5d588d2cd9b9866
Victor Rene Morales Zepeda <vicmoze95@gmail.com>
2023-10-28 13:23:57 -0600

    Agregando clases y pom

---
+++ src/main/java/com/rabbitmq/ejemploRabbit/EjemploRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.ejemploRabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploRabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbitmq/ejemploRabbit/EjemploRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.ejemploRabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploRabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
